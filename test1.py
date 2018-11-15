5cf9ce72c7a579018516a0d5966e20695a0763a1
Gianmarco Garrisi <gianmarcogarrisi@tutanota.com>
2018-11-15 11:39:57 +0100

    Use MFCC as it has lower dimensionality

--- test1.py
+++ test1.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
 Created on Fri Nov  2 11:50:53 2018
@@ -11,37 +12,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 from os import listdir, walk
 
-X=[]
+X = []
 lang_idx = 0
 y = np.zeros(0)
 lables = {}
-for dir, _, files in walk("eng_test"):
+for dir, _, files in walk("training_data"):
     for f in files:
-        print(dir)
-        d,sr=librosa.load(dir+"\\" + f)
-        #spectrogram
-        D = librosa.amplitude_to_db(
-                np.abs(librosa.stft(d,
-                                    n_fft=184, hop_length=96)),
-                                    ref=np.max)
-        CQT = librosa.amplitude_to_db(np.abs(librosa.cqt(d, sr=sr)), ref=np.max)
-        X.append(D)
-        if dir not in lables:
-            lables[dir]=lang_idx
-            lang_idx+=1
+        if f[-3:] != "wav":
+            continue
+        # print(dir)
+        d, sr = librosa.load(dir + "/" + f)
         y = np.append(y, lables[dir])
-        #mfcc
-        #mfccs = librosa.feature.mfcc(y=d, sr=sr, n_mfcc=40) 
+        # mfcc
+        mfccs = librosa.feature.mfcc(y=d, sr=sr, n_mfcc=40)
+        print(mfccs.shape)
+        X.append(mfccs)
+        if dir not in lables:
+            lables[dir] = lang_idx
+            lang_idx += 1
 
-idx=0
+idx = 0
 min = 100000000
 for a in X:
     if a.shape[1] < min:
         min = a.shape[1]
 
-x=np.zeros((len(X), 93, min))
+x = np.zeros((len(X), 93, min))
 for arr in X:
     x[idx, :, :] = arr[:, :min]
-    idx +=1
-    
-    
+    idx += 1
