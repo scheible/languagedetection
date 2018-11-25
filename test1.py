844ac241f70aceb5807d226b8a36f29313b14482
Gianmarco Garrisi <gianmarcogarrisi@tutanota.com>
2018-11-25 16:22:17 +0100

    Fix

--- test1.py
+++ test1.py
@@ -6,9 +6,10 @@ Created on Fri Nov  2 11:50:53 2018
 @author: qiany
 """
 import librosa
+import numpy as np
 from librosa import display
 import matplotlib
-import numpy as np
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 from os import listdir, walk
 
@@ -20,17 +21,14 @@ for dir, _, files in walk("training_data"):
     for f in files:
         if f[-3:] != "wav":
             continue
-        # print(dir)
         d, sr = librosa.load(dir + "/" + f)
-        y = np.append(y, lables[dir])
         # mfcc
         mfccs = librosa.feature.mfcc(y=d, sr=sr, n_mfcc=40)
-        print(mfccs.shape)
         X.append(mfccs)
         if dir not in lables:
             lables[dir] = lang_idx
             lang_idx += 1
-
+        y = np.append(y, lables[dir])
 idx = 0
 min = 100000000
 for a in X:
