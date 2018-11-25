965ce2372e0c6b62c80984e398522360f665d3f1
qyh7 <qianyunhu7@gmail.com>
2018-11-25 16:42:27 +0100

    defind input as a function

---
+++ input_para.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  2 11:50:53 2018
+
+@author: qiany
+"""
+import librosa
+import numpy as np
+from librosa import display
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+from os import listdir, walk
+
+def function(folder):
+    X = []
+    lang_idx = 0
+    y = np.zeros(0)
+    lables = {}
+    for dir, _, files in walk(folder):
+        for f in files:
+            if f[-3:] != "wav":
+                continue
+            d, sr = librosa.load(dir + "/" + f)
+            # mfcc
+            mfccs = librosa.feature.mfcc(y=d, sr=sr, n_mfcc=40)
+            X.append(mfccs)
+            if dir not in lables:
+                lables[dir] = lang_idx
+                lang_idx += 1
+            y = np.append(y, lables[dir])
+    idx = 0
+    min = 100000000
+    for a in X:
+        if a.shape[1] < min:
+            min = a.shape[1]
+    
+    x = np.zeros((len(X), 40, min))
+    for arr in X:
+        x[idx, :, :] = arr[:, :min]
+        idx += 1
+    
+    return (x, y)
